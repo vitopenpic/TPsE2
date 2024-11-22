56dc834bc163c5c4ed3a848e9cd469045520a5a8
aenriccilouzan <aenriccilouzan@itba.edu.ar>
2024-11-21 21:42:11 -0300

    aaaaa iaia

--- tp3/verilog_andre/modulo_ingresar_numero.v
+++ tp3/verilog_andre/modulo_ingresar_numero.v
@@ -5,7 +5,7 @@ module modulo_ingresar_numero (clk, reset, ingresar_numero_1_en, contador, opera
     //output [2:1] y;         // State output (para debug)
     output reg[1:0] contador;
     reg [1:0] curr_state, next_state;
-    reg [15:0] numero;
+    reg [15:0] numero_1;
     reg in_prev;
     reg detector;
     // Asignacion de estados
@@ -15,10 +15,10 @@ module modulo_ingresar_numero (clk, reset, ingresar_numero_1_en, contador, opera
     parameter [1:0] Enable= 2'b11;
 
     // Logica de proximo estado (combinacional)
-    always @(in, curr_state)
+    always @(clk, ingresar_numero_en)
         case (curr_state)
             Esperar: begin 
-                    if (ingresar_numero_1_en == 1 && ingresar_numero_en == 1 && detect==1) begin
+                    if (ingresar_numero_1_en == 1 && ingresar_numero_en == 1 && detector==1) begin
                         next_state <= Enable;
                     end   
                     else begin 
@@ -30,10 +30,10 @@ module modulo_ingresar_numero (clk, reset, ingresar_numero_1_en, contador, opera
 
                 end
             Mostrar_numero: begin 
-                    if(dectector==1) ;//solamente hacelo si sigue la entrada de clock activa
+                    if(detector==1) ;//solamente hacelo si sigue la entrada de clock activa
                     begin
-                    numero<=numero<<4; //corro el numero 4 veces
-                    numero[3:0]<=nuevo_numero;
+                    numero_1<=numero_1<<4; //corro el numero 4 veces
+                    numero_1[3:0]<=nuevo_numero;
                     if (ingresar_numero_1_en == 0 && contador == 4) next_state <= Operando;
                     else if (ingresar_numero_1_en == 0 && contador < 4) next_state <= Esperar;
                     else next_state <= Mostrar_numero;
@@ -48,19 +48,20 @@ module modulo_ingresar_numero (clk, reset, ingresar_numero_1_en, contador, opera
         endcase
 
     // Transicion al proximo estado (secuencial)
-    always @(posedge clk)
-        if(reset==1);begin
+    always @(posedge reset)
+        if(reset==1)begin
             in_prev<=0;
-            dectector<=0;
+            detector<=0;
             curr_state <= Esperar;
+            contador<=0;
         end
         else begin 
             curr_state <= next_state;
 			if(numero_en && !in_prev) //se fija el anterior y el nuevo a ver si cambio si si, detecta el flanco entonces podes ir a la logica
                 begin //sino, no detecta y no hace nada
-                    detect<=1;  
+                    detector<=1;  
                 end
-            else detect<=0;
+            else detector<=0;
         end
 
     // Salida (combinacional)
@@ -89,5 +90,5 @@ module modulo_ingresar_numero (clk, reset, ingresar_numero_1_en, contador, opera
 
                     enable<=0;
 				end
-		end
+		
 endmodule
